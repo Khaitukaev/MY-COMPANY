83b021635b58acdd6f9af942f88a63c71980dade
Khaitukaev <100046187+Khaitukaev@users.noreply.github.com>
2022-06-11 18:03:01 +0300

    Initial Commit

---
+++ COMPANY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COMPANY
+//
+//  Created by Акраман Хайтукаев on 11.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
